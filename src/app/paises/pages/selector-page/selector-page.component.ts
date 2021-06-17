568f09c9895a1041bbd7c5a17b0b45c058c3e5e3
Marcos <marcos.ledesma.picazo@cieep.com>
2021-06-17 10:25:14 +0200

    Cambiar códigos por nombres

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -22,7 +22,8 @@ export class SelectorPageComponent implements OnInit {
   // llenar selectores
   regiones: string[] = [];
   paises: PaisSmall[] = [];
-  fronteras: string[] = [];
+  // fronteras: string[] = [];
+  fronteras: PaisSmall[] = [];
 
   // UI
   cargando: boolean = false;
@@ -53,10 +54,12 @@ export class SelectorPageComponent implements OnInit {
         this.miFormulario.get('frontera')?.reset('');
         this.cargando = true;
       }),
-      switchMap( codigo => this.paisesService.getPaisPorCodigo( codigo ) )
+      switchMap( codigo => this.paisesService.getPaisPorCodigo( codigo ) ),
+      switchMap( pais => this.paisesService.getPaisesPorCodigos( pais?.borders! ) )
     )
-    .subscribe( pais => {
-        this.fronteras = pais?.borders || [] ;
+    .subscribe( paises => {
+        // this.fronteras = pais?.borders || [] ;
+        this.fronteras = paises;
         this.cargando = false;
       })
   }
--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { combineLatest, Observable, of } from 'rxjs';
 import { Pais, PaisSmall } from '../interfaces/paises.interface';
 
 @Injectable({
@@ -24,13 +24,33 @@ export class PaisesService {
   }
 
   getPaisPorCodigo( codigo: string ): Observable<Pais | null> {
-
     if( !codigo ){
       return of(null)
     }
 
     const url = `${ this.baseUrl }/alpha/${ codigo }`;
     return this.http.get<Pais>( url );
+  }
+
+  getPaisPorCodigoSmall( codigo: string ): Observable<PaisSmall> {
+    const url = `${ this.baseUrl }/alpha/${ codigo }?fields=alpha3Code;name`;
+    return this.http.get<PaisSmall>( url );
+  }
+
+  getPaisesPorCodigos( borders: string[] ): Observable<PaisSmall[]> {
+    if( !borders ){
+      return of([]);
+    }
+
+    const peticiones: Observable<PaisSmall>[] = [];
+
+    borders.forEach( codigo => {
+      const peticion = this.getPaisPorCodigoSmall(codigo);
+      peticiones.push( peticion );
+    });
+
+    return combineLatest ( peticiones );
+
   }
 
 }
